d2c5c5f0751264d39da56710d3a34bee74177b59
Rohit Kumar <rohit.r.kumar@newgensoft.com>
2024-04-05 15:07:08 +0530

    navbar with  responsiveness setup using mui

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,55 @@
+import { Groups, Home, ModeNight, Pages, Settings } from '@mui/icons-material'
+import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Switch } from '@mui/material'
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+  <Box  flex={1} p={2} sx={{display:{xs: "none", sm: "block"}}}>
+            <List>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#home">
+              <ListItemIcon>
+                <Home/>
+              </ListItemIcon>
+              <ListItemText primary="HomePage" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#pages">
+              <ListItemIcon>
+                <Pages/>
+              </ListItemIcon>
+              <ListItemText primary="Pages" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#groups">
+              <ListItemIcon>
+                <Groups/>
+              </ListItemIcon>
+              <ListItemText primary="Groups" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#setting">
+              <ListItemIcon>
+                <Settings />
+              </ListItemIcon>
+              <ListItemText primary="Setting" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#mode">
+              <ListItemIcon>
+                <ModeNight />
+          </ListItemIcon>
+                <Switch />
+
+            </ListItemButton>
+          </ListItem>
+          </List>
+  </Box>
+  )
+}
+
+export default Sidebar
